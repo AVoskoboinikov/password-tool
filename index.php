c9b64d575c393c3f861b93d9d35d7af7bfbd988f
azvo <avoskoboinikov@magento.com>
2017-03-06 00:37:18 +0200

    updates

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 $secret1 = null;
 $secret2 = null;
-$passwordLength = '12';
+$passwordLength = '15';
 
 $dictionary = [
     'Q','W','E','R','T','Y','U','I','O','P','A','S','D',
@@ -11,10 +11,10 @@ $dictionary = [
     'q','w','e','r','t','y','u','i','o','p','a','s','d',
     'f','g','h','j','k','l','z','x','c','v','b','n','m',
 
-    '1','2','3','4','5','6','7','8','9','0',
+    '1','2','3','4','5','6','7','8','9',
 
     '~','!','@','#','$','%','^','&','*','(',')','_','+',
-    ',','.',':',';','?','[',']','<','>','-','='
+    ',','.',':',';','?','[',']','-','='
 ];
 
 $options = getopt('', ['s1:','s2:']);
@@ -27,19 +27,28 @@ $secret1 = $options['s1'];
 $secret2 = $options['s2'];
 
 $password = [];
-$secret1 = str_split(hash('sha256', $secret1));
-$secret2 = str_split(hash('sha256', $secret2));
+$hashedSecret1 = str_split(hash('sha256', $secret1));
+$hashedSecret2 = str_split(hash('sha256', $secret2));
 
-$dictionarySize = gmp_init(count($dictionary)); // 86
-$primitiveRoot = gmp_init(3); // primitive root for 86
+$secret1 = str_split($secret1);
+$secret2 = str_split($secret2);
+
+$dictionarySize = gmp_init(count($dictionary)); // 83
+$primitiveRoot = gmp_init(2); // primitive root for 83
 
 for ($i=0; $i<$passwordLength; $i++) {
-    $char1 = $secret1[$i];
-    $char2 = $secret2[$i];
+    $hashChar1 = $hashedSecret1[$i % count($hashedSecret1)];
+    $hashChar2 = $hashedSecret2[$i % count($hashedSecret2)];
+
+    $secretChar1 = $secret1[$i % count($secret1)];
+    $secretChar2 = $secret2[$i % count($secret2)];
 
     $code = (int) gmp_powm(
         $primitiveRoot,
-        gmp_pow(ord($char1), ord($char2)),
+        gmp_pow(
+            ord($hashChar1) % ord($secretChar1),
+            ord($hashChar2) % ord($secretChar2)
+        ),
         $dictionarySize
     );
 
