00d093fba02f36f6f93d2a531636ba40d7f87660
azvo <avoskoboinikov@magento.com>
2017-03-11 22:28:00 +0200

    updates

--- web/js/main.js
+++ web/js/main.js
@@ -1,9 +1,21 @@
 document.getElementById('generate-button').addEventListener('click', function () {
     var secret_1 = document.getElementById('secret-1').value,
         secret_2 = document.getElementById('secret-2').value,
+        password_length = document.getElementById('password-length').value,
         password;
 
-    password = generateFancyPassword(secret_1, secret_2);
+    password = generateFancyPassword(secret_1, secret_2, password_length);
 
     document.getElementById('password').value = password;
+});
+
+document.getElementById('show-passwords-checkbox').addEventListener('click', function (e) {
+    var type = 'password';
+
+    if (e.toElement.checked) {
+        type = 'text';
+    }
+
+    document.getElementById('secret-1').setAttribute('type', type);
+    document.getElementById('secret-2').setAttribute('type', type);
 });
