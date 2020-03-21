d09e842e5bbaaf199bcc808b18651543135eab80
AVoskoboinikov <avoskoboinikov@magento.com>
2020-03-21 15:25:17 +0100

    add generator v2

--- web/js/main.js
+++ web/js/main.js
@@ -2,9 +2,17 @@ document.getElementById('generate-button').addEventListener('click', function ()
     var secret_1 = document.getElementById('secret-1').value,
         secret_2 = document.getElementById('secret-2').value,
         password_length = document.getElementById('password-length').value,
+        useV2 = document.getElementById('use-generator-v2').checked,
         password;
 
-    password = generateFancyPassword(secret_1, secret_2, password_length);
+    if (secret_1.length === 0) return;
+    if (secret_2.length === 0) return;
+
+    if (useV2) {
+        password = generateFancyPasswordV2(secret_1, secret_2, password_length);
+    } else {
+        password = generateFancyPassword(secret_1, secret_2, password_length);
+    }
 
     document.getElementById('password').value = password;
 });
